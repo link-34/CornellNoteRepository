e7481ae1f8cb312eaea580c52b6db5a0622f9852
link-3434 <link-3434@example.com>
2022-01-10 22:23:13 +0900

    「User.java」のインデントをTAB→スペースへ修正

--- src/main/java/com/example/cornellnote/domain/model/User.java
+++ src/main/java/com/example/cornellnote/domain/model/User.java
@@ -6,8 +6,8 @@ import lombok.Data;
 public class User {
 
     private int	        userId;         // ID
-	private String     userName;       // ユーザー名
-	private String     userPass;       // パスワード
-	private byte       isDeleted;      // 削除フラグ(0:未削除、  1:削除済み)
-	private byte       isAdmin;        // 管理者権限(0:権限なし、1:権限あり)
+    private String     userName;       // ユーザー名
+    private String     userPass;       // パスワード
+    private byte       isDeleted;      // 削除フラグ(0:未削除、  1:削除済み)
+    private byte       isAdmin;        // 管理者権限(0:権限なし、1:権限あり)
 }
