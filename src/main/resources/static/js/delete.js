506c00fca727f5e4cf248ba0fa074b3c08ab1388
link-3434 <link-3434@example.com>
2022-01-17 21:52:51 +0900

    jsファイルによるダイアログ実装

---
+++ src/main/resources/static/js/delete.js
@@ -0,0 +1,11 @@
+// window.alert('test');
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('delete').addEventListener('click', function(e) {
+        if(!window.confirm('本当に削除しますか？')) {
+            e.preventDefault();
+        } else {
+            // 処理なし
+        }
+    }, false);
+}, false);
