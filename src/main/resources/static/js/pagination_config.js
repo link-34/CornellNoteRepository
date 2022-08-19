2ae46c2ef22de5c2fb70950023726fc2ce112da1
link-34 <the_legend_of_zelda_34@yahoo.co.jp>
2022-08-20 05:38:50 +0900

    インデント微修正

--- src/main/resources/static/js/pagination_config.js
+++ src/main/resources/static/js/pagination_config.js
@@ -1,11 +1,11 @@
 $(function() {
 	$('.index-pagination').paginathing({		// 親要素のclassを記述
-		perPage:	3,							// 1ページあたりの表示件数
-		limitPagination:5,						// ページネーション数を制限
-		prevText:	'前へ',						// 1つ前のページへ移動するボタンのテキスト
-		nextText:	'次へ',						// 1つ次のページへ移動するボタンのテキスト
-		firstText:	'最初へ',					// 「最初のボタン」テキスト
-		lastText:	'最後へ',					// 「最後のボタン」のテキスト
-		pageNumbers: true						// 総ページ数のうち現在のページ数を表示
+		perPage:			3,					// 1ページあたりの表示件数
+		limitPagination:	5,					// ページネーション数を制限
+		prevText:			'前へ',				// 1つ前のページへ移動するボタンのテキスト
+		nextText:			'次へ',				// 1つ次のページへ移動するボタンのテキスト
+		firstText:			'最初へ',			// 「最初のボタン」テキスト
+		lastText:			'最後へ',			// 「最後のボタン」のテキスト
+		pageNumbers:		true				// 総ページ数のうち現在のページ数を表示
 	})
 });
